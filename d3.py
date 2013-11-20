f2c59221861d0a613339bde59b400f65deb1ef58
alex-nexus <alkchen@gmail.com>
2013-11-19 22:45:01 -0800

    choropleths

--- d3.py
+++ d3.py
@@ -7,9 +7,5 @@ app = Flask(__name__)
 def hello():
   return 'hello world!'
 
-@app.route("/day_hour_heatmap")
-def day_hour_heatmap():
-  return render_template('day_hour_heatmap/index.html')
-
 if __name__ == "__main__":
   app.run()
